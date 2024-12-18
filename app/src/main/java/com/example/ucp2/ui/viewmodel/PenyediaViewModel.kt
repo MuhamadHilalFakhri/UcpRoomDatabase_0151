d38d2da3867c981ed28f26f8f1df75b7113f2a22
hilal555 <muhamadhilal04@gmail.com>
2024-12-18 18:59:17 +0700

    membuat PenyediaViewModel

---
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/PenyediaViewModel.kt
@@ -0,0 +1,27 @@
+package com.example.ucp2.ui.viewmodel
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.ucp2.SisforApp
+
+object PenyediaViewModel {
+    val Factory = viewModelFactory {
+        initializer {
+            DosenViewModel(
+                sisforApp().containerApp.repositoryDosen
+            )
+        }
+        initializer {
+            DetailDosenViewModel(
+                createSavedStateHandle(),
+                sisforApp().containerApp.repositoryDosen
+            )
+        }
+    }
+}
+
+fun CreationExtras.sisforApp(): SisforApp =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as SisforApp)

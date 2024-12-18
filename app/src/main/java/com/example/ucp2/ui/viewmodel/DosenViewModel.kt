e982b67e8e3b4ffcbced0df54d6023688b4677c0
hilal555 <muhamadhilal04@gmail.com>
2024-12-18 18:06:51 +0700

    membuat dataClass DosenEvent

--- app/src/main/java/com/example/ucp2/ui/viewmodel/DosenViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/DosenViewModel.kt
@@ -5,4 +5,10 @@ import com.example.ucp2.repository.RepositoryDosen
 
 class DosenViewModel (private val repositoryDosen: RepositoryDosen) : ViewModel()
 {
+    // Data class untuk menyimpan data input form
+    data class DosenEvent(
+        val nidn: String = "",
+        val nama: String = "",
+        val jenisKelamin: String = ""
+    )
 }

78e8278dddf74bf81794c82fa4b555d52c189707
hilal555 <muhamadhilal04@gmail.com>
2024-12-18 11:06:39 +0700

    membuat beberapa file dalam package

---
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/MataKuliahViewModel.kt
@@ -0,0 +1,16 @@
+package com.example.ucp2.ui.viewmodel
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.ucp2.data.entity.MataKuliah
+import com.example.ucp2.repository.RepositoryMK
+import kotlinx.coroutines.launch
+
+class MataKuliahViewModel(private val repositoryMK: RepositoryMK) : ViewModel()
+{
+
+
+}

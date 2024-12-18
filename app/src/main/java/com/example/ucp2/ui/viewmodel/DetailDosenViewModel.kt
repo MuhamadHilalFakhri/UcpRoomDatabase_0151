22ff08ff89753381f6e314194b0a19452b47fe38
hilal555 <muhamadhilal04@gmail.com>
2024-12-18 18:37:48 +0700

    menambahkan fungs untuk memindahkan data dari entity ke ui

--- app/src/main/java/com/example/ucp2/ui/viewmodel/DetailDosenViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/DetailDosenViewModel.kt
@@ -2,6 +2,14 @@ package com.example.ucp2.ui.viewmodel
 import com.example.ucp2.ui.viewmodel.DosenViewModel.DosenEvent
 import com.example.ucp2.data.entity.Dosen
 
+// Memindahkan data dari entity ke UI
+fun Dosen.toDetailUiEvent(): DosenEvent {
+    return DosenEvent(
+        nidn = nidn,
+        nama = nama,
+        jenisKelamin = jenisKelamin
+    )
+}
 
 data class DosenEvent(
     val nidn: String = "",

a582f44f3ced5c34f01e980ae41b46b2ab19b947
hilal555 <muhamadhilal04@gmail.com>
2024-12-18 19:49:44 +0700

    membuat fungsi BodyDetailDosen

--- app/src/main/java/com/example/ucp2/ui/view/dosen/DetailDosenView.kt
+++ app/src/main/java/com/example/ucp2/ui/view/dosen/DetailDosenView.kt
@@ -1,11 +1,13 @@
 package com.example.ucp2.ui.view.dosen
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -16,6 +18,51 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.ucp2.data.entity.Dosen
+import com.example.ucp2.ui.viewmodel.DetailUiState
+import com.example.ucp2.ui.viewmodel.toDosenEntity
+
+@Composable
+fun BodyDetailDosen(
+    modifier: Modifier = Modifier,
+    detailUiState: DetailUiState = DetailUiState(),
+){
+    when{
+        detailUiState.isLoading ->{
+            Box(
+                modifier = modifier.fillMaxWidth(),
+                contentAlignment = Alignment.Center
+            ){
+                CircularProgressIndicator()
+            }
+        }
+
+        detailUiState.isUiEventNotEmpty ->{
+            Column(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            ) {
+                ItemDetailDosen(
+                    dosen = detailUiState.detailUiEvent.toDosenEntity(),
+                    modifier = Modifier
+                )
+            }
+        }
+
+        detailUiState.isUiEventNotEmpty ->{
+            Box(
+                modifier = modifier.fillMaxWidth(),
+                contentAlignment = Alignment.Center
+            ){
+                Text(
+                    text = "Data tidak ditemukan",
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        }
+    }
+}
+
 
 @Composable
 fun ItemDetailDosen(

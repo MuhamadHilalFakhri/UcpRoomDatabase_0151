f585139d9ab3b7fe136e4ae30cb3b6cde1213c45
hilal555 <muhamadhilal04@gmail.com>
2024-12-18 18:37:22 +0700

    menambahkan data class DosenEvent

--- app/src/main/java/com/example/ucp2/ui/viewmodel/DetailDosenViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/DetailDosenViewModel.kt
@@ -3,6 +3,11 @@ import com.example.ucp2.ui.viewmodel.DosenViewModel.DosenEvent
 import com.example.ucp2.data.entity.Dosen
 
 
+data class DosenEvent(
+    val nidn: String = "",
+    val nama: String = "",
+    val jenisKelamin: String = ""
+)
 fun DosenEvent.toDosenEntity(): Dosen = Dosen(
     nidn = nidn,
     nama = nama,

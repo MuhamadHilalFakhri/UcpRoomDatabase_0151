23ea91a78b9d76cafc8205bb069a6d322c1ab1e1
hilal555 <muhamadhilal04@gmail.com>
2024-12-18 19:05:42 +0700

    membuat fun BodyHomeDosenView

--- app/src/main/java/com/example/ucp2/ui/view/dosen/HomeDosenView.kt
+++ app/src/main/java/com/example/ucp2/ui/view/dosen/HomeDosenView.kt
@@ -1,10 +1,14 @@
 package com.example.ucp2.ui.view.dosen
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Home
@@ -12,14 +16,90 @@ import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.ucp2.data.entity.Dosen
+import com.example.ucp2.ui.viewmodel.DosenViewModel
+import kotlinx.coroutines.launch
+
+
+@Composable
+fun BodyHomeDosenView(
+    dosenUiState: DosenViewModel.DosenUIState,
+    onClick: (String) -> Unit = { },
+    modifier: Modifier = Modifier
+) {
+    val coroutineScope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() }
+
+    when {
+        dosenUiState.snackBarMessage != null -> {
+            // Menampilkan pesan Snackbar
+            LaunchedEffect(dosenUiState) {
+                dosenUiState.snackBarMessage?.let { message ->
+                    coroutineScope.launch {
+                        snackbarHostState.showSnackbar(message)
+                    }
+                }
+            }
+        }
+        dosenUiState.dosenList.isEmpty() -> {
+            // Menampilkan pesan jika data kosong
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(
+                    text = "Tidak ada data dosen.",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        }
+
+        else -> {
+            // Menampilkan daftar dosen
+            ListDosen(
+                listDosen = dosenUiState.dosenList,
+                onClick = { onClick(it) },
+                modifier = modifier
+            )
+        }
+    }
+}
+
+
+@Composable
+fun ListDosen(
+    listDosen: List<Dosen>,
+    modifier: Modifier = Modifier,
+    onClick: (String) -> Unit = { }
+) {
+    LazyColumn(
+        modifier = modifier
+    ) {
+        items(
+            items = listDosen,
+            itemContent = { dosen ->
+                CardDosen(
+                    dosen = dosen,
+                    onClick = { onClick(dosen.nidn) }
+                )
+            }
+        )
+    }
+}
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable

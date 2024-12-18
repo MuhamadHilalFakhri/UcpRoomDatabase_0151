74f7deac054ef297dc75a661e7272a0378c37345
hilal555 <muhamadhilal04@gmail.com>
2024-12-18 17:55:56 +0700

    membuat object DestinasiDetailDosenDosen

--- app/src/main/java/com/example/ucp2/ui/navigation/AlamatNavigasi.kt
+++ app/src/main/java/com/example/ucp2/ui/navigation/AlamatNavigasi.kt
@@ -7,3 +7,9 @@ interface AlamatNavigasi {
 object DestinasiHomeDosen : AlamatNavigasi {
     override val route = "home_dosen"
 }
+
+object DestinasiDetailDosen : AlamatNavigasi {
+    override val route = "detail_dosen"
+    const val NIDN = "nidn"
+    val routesWithArg = "$route/{$NIDN}"
+}

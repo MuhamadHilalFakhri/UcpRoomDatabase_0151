053c8b1d4080216a19a4115ed9968a0772d2b624
hilal555 <muhamadhilal04@gmail.com>
2024-12-18 19:13:02 +0700

    membuat data class HomeUiState

---
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/HomeDosenViewModel.kt
@@ -0,0 +1,11 @@
+package com.example.ucp2.ui.viewmodel
+
+import com.example.ucp2.data.entity.Dosen
+
+
+data class HomeUiState(
+    val listDosen: List<Dosen> = listOf(),
+    val isLoading: Boolean = false,
+    val isError: Boolean = false,
+    val errorMessage: String = ""
+)

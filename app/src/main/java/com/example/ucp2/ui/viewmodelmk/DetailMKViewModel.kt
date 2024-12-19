eadb8377262b9e348d96bee62e47c76574090acc
hilal555 <muhamadhilal04@gmail.com>
2024-12-19 14:18:43 +0700

    Membuat data class MataKuliahEvent

---
+++ app/src/main/java/com/example/ucp2/ui/viewmodelmk/DetailMKViewModel.kt
@@ -0,0 +1,9 @@
+package com.example.ucp2.ui.viewmodelmk
+
+data class MataKuliahEvent(
+    val kode: String = "",
+    val nama: String = "",
+    val sks: String = "",
+    val semester: String = "",
+    val dosenPengampu: String = ""
+)

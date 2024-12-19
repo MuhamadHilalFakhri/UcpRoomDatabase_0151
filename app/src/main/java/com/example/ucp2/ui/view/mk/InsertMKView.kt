335788fe68431d8e24f2a613397d76323c1bf01c
hilal555 <muhamadhilal04@gmail.com>
2024-12-19 20:24:19 +0700

    Membuat fungsi InsertMatkulView

--- app/src/main/java/com/example/ucp2/ui/view/mk/InsertMKView.kt
+++ app/src/main/java/com/example/ucp2/ui/view/mk/InsertMKView.kt
@@ -3,18 +3,89 @@ package com.example.ucp2.ui.view.mk
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.ucp2.ui.customwidget.CustomTopAppBar
+import com.example.ucp2.ui.navigation.AlamatNavigasi
+import com.example.ucp2.ui.viewmodeldosen.PenyediaViewModel
 import com.example.ucp2.ui.viewmodelmk.MataKuliahViewModel
 import com.example.ucp2.ui.viewmodelmk.MataKuliahViewModel.MataKuliahEvent
 import com.example.ucp2.ui.viewmodelmk.MataKuliahViewModel.FormErrorState
+import kotlinx.coroutines.launch
+
+object DestinasiInsertMatkul : AlamatNavigasi {
+    override val route: String = "insert_matkul"
+}
+
+@Composable
+fun InsertMatkulView(
+    onBack: () -> Unit,
+    onNavigate: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: MataKuliahViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val uiState = viewModel.uiStateMK // Get UI state from ViewModel
+    val snackbarHostState = remember { SnackbarHostState() }
+    val coroutineScope = rememberCoroutineScope()
+
+    // Observe changes in snackbarMessage
+    LaunchedEffect(uiState.snackBarMessage) {
+        uiState.snackBarMessage?.let { message ->
+            coroutineScope.launch {
+                snackbarHostState.showSnackbar(message)
+                viewModel.resetSnackBarMessageMK()
+            }
+        }
+    }
+
+    Scaffold(
+        modifier = modifier,
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) }
+    ) { padding ->
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(padding)
+                .padding(16.dp)
+        ) {
+            CustomTopAppBar(
+                onBack = onBack,
+                showBackButton = true,
+                judul = "Tambah Mata Kuliah"
+            )
+
+            // Body
+            InsertBodyMatkul(
+                uiState = uiState,
+                onValueChange = { updateEvent ->
+                    viewModel.updateStateMK(updateEvent)
+                },
+                onClick = {
+                    coroutineScope.launch {
+                        viewModel.saveDataMK() // Save data
+                    }
+                    onNavigate()
+                }
+            )
+        }
+    }
+}
 
 @Composable
 fun InsertBodyMatkul(

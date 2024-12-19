ded2d24c2f209fe97f52a8372da350818560058a
hilal555 <muhamadhilal04@gmail.com>
2024-12-19 14:31:48 +0700

    Membuat fungsi untuk menyimpan input form ke dalam entity

--- app/src/main/java/com/example/ucp2/ui/viewmodelmk/MataKuliahViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodelmk/MataKuliahViewModel.kt
@@ -1,10 +1,18 @@
 package com.example.ucp2.ui.viewmodelmk
 
 import androidx.lifecycle.ViewModel
+import com.example.ucp2.data.entity.MataKuliah
 import com.example.ucp2.repository.RepositoryMK
 
 class MataKuliahViewModel(private val repositoryMK: RepositoryMK) : ViewModel()
 {
 
-
+    // Menyimpan input form ke dalam entity
+    fun MataKuliahEvent.toMataKuliahEntity(): MataKuliah = MataKuliah(
+        kode = kode,
+        nama = nama,
+        sks = sks,
+        semester = semester,
+        dosenPengampu = dosenPengampu
+    )
 }

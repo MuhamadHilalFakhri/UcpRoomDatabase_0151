b7eac28a81ff965379ff376a005f918c8d28ddbe
hilal555 <muhamadhilal04@gmail.com>
2024-12-19 20:20:46 +0700

    Membuat fungsi FormMatkul

---
+++ app/src/main/java/com/example/ucp2/ui/view/mk/InsertMKView.kt
@@ -0,0 +1,86 @@
+package com.example.ucp2.ui.view.mk
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
+import com.example.ucp2.ui.viewmodelmk.MataKuliahViewModel.MataKuliahEvent
+import com.example.ucp2.ui.viewmodelmk.MataKuliahViewModel.FormErrorState
+
+
+@Composable
+fun FormMatkul(
+    mataKuliahEvent: MataKuliahEvent = MataKuliahEvent(),
+    onValueChange: (MataKuliahEvent) -> Unit,
+    errorState: FormErrorState = FormErrorState(),
+    modifier: Modifier = Modifier
+) {
+    Column(modifier = modifier.fillMaxWidth()) {
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = mataKuliahEvent.kode,
+            onValueChange = {
+                onValueChange(mataKuliahEvent.copy(kode = it))
+            },
+            label = { Text("Kode Mata Kuliah") },
+            isError = errorState.kode != null,
+            placeholder = { Text("Masukkan Kode Mata Kuliah") },
+        )
+        Text(text = errorState.kode ?: "", color = Color.Red)
+
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = mataKuliahEvent.nama,
+            onValueChange = {
+                onValueChange(mataKuliahEvent.copy(nama = it))
+            },
+            label = { Text("Nama Mata Kuliah") },
+            isError = errorState.nama != null,
+            placeholder = { Text("Masukkan Nama Mata Kuliah") },
+        )
+        Text(text = errorState.nama ?: "", color = Color.Red)
+
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = mataKuliahEvent.sks,
+            onValueChange = {
+                onValueChange(mataKuliahEvent.copy(sks = it))
+            },
+            label = { Text("SKS") },
+            isError = errorState.sks != null,
+            placeholder = { Text("Masukkan SKS") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        )
+        Text(text = errorState.sks ?: "", color = Color.Red)
+
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = mataKuliahEvent.semester,
+            onValueChange = {
+                onValueChange(mataKuliahEvent.copy(semester = it))
+            },
+            label = { Text("Semester") },
+            isError = errorState.semester != null,
+            placeholder = { Text("Masukkan Semester") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        )
+        Text(text = errorState.semester ?: "", color = Color.Red)
+
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = mataKuliahEvent.dosenPengampu,
+            onValueChange = {
+                onValueChange(mataKuliahEvent.copy(dosenPengampu = it))
+            },
+            label = { Text("Dosen Pengampu") },
+            isError = errorState.dosenPengampu != null,
+            placeholder = { Text("Masukkan Dosen Pengampu") },
+        )
+        Text(text = errorState.dosenPengampu ?: "", color = Color.Red)
+    }
+}

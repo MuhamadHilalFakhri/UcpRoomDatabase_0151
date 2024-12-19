ab3584c67bc48c531e7c2868d2224363223e25c8
hilal555 <muhamadhilal04@gmail.com>
2024-12-19 15:19:22 +0700

    melengkapi PenyediaViewModel

--- app/src/main/java/com/example/ucp2/ui/viewmodeldosen/PenyediaViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodeldosen/PenyediaViewModel.kt
@@ -1,11 +1,15 @@
 package com.example.ucp2.ui.viewmodeldosen
 
+import UpdateMatkulViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.ucp2.SisforApp
+import com.example.ucp2.ui.viewmodelmk.DetailMataKuliahViewModel
+import com.example.ucp2.ui.viewmodelmk.HomeMatkulViewModel
+import com.example.ucp2.ui.viewmodelmk.MataKuliahViewModel
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
@@ -24,6 +28,29 @@ object PenyediaViewModel {
                 createSavedStateHandle(),
                 sisforApp().containerApp.repositoryDosen
             )
+        }
+        initializer {
+            MataKuliahViewModel(
+                sisforApp().containerApp.repositoryMK
+            )
+        }
+        initializer {
+            HomeMatkulViewModel(
+                sisforApp().containerApp.repositoryMK
+            )
+        }
+        initializer {
+            DetailMataKuliahViewModel(
+                createSavedStateHandle(),
+                sisforApp().containerApp.repositoryMK
+            )
+        }
+        initializer {
+            UpdateMatkulViewModel(
+                createSavedStateHandle(),
+                sisforApp().containerApp.repositoryMK,
+
+                )
         }
     }
 }

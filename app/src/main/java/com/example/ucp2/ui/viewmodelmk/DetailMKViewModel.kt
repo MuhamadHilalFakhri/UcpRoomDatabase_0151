e2fbf0c8b9212b261d608bd436b6d91f42b4409a
hilal555 <muhamadhilal04@gmail.com>
2024-12-19 14:19:14 +0700

    Membuat fun MataKuliahEvent

--- app/src/main/java/com/example/ucp2/ui/viewmodelmk/DetailMKViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodelmk/DetailMKViewModel.kt
@@ -1,5 +1,14 @@
 package com.example.ucp2.ui.viewmodelmk
 
+import com.example.ucp2.data.entity.MataKuliah
+
+fun MataKuliahEvent.toMataKuliahEntity(): MataKuliah = MataKuliah(
+    kode = kode,
+    nama = nama,
+    sks = sks,
+    semester = semester,
+    dosenPengampu = dosenPengampu
+)
 data class MataKuliahEvent(
     val kode: String = "",
     val nama: String = "",

8fd7f51b1331b489410da5d8fbd5e32f055bbfdb
hilal555 <muhamadhilal04@gmail.com>
2024-12-19 14:10:23 +0700

    membuat AlamatNavigasiMK

---
+++ app/src/main/java/com/example/ucp2/ui/navigation/AlamatNavigasiMK.kt
@@ -0,0 +1,21 @@
+package com.example.ucp2.ui.navigation
+
+interface AlamatNavigasiMK {
+    val route: String
+}
+
+object DestinasiHomeMatkul : AlamatNavigasiMK {
+    override val route = "homeMatkul"
+}
+
+object DestinasiDetailMatkul : AlamatNavigasiMK {
+    override val route = "detailMatkul"
+    const val KODE = "kode"
+    val routesWithArg = "$route/{$KODE}"
+}
+
+object DestinasiUpdateMatkul : AlamatNavigasiMK {
+    override val route = "updateMatkul"
+    const val KODE = "kode"
+    val routesWithArg = "$route/{$KODE}"
+}

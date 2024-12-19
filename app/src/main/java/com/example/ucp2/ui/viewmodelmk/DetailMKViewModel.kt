6f714e86c3e487ff69e64c98d47e050aef802884
hilal555 <muhamadhilal04@gmail.com>
2024-12-19 14:21:37 +0700

    Membuat fungsi untuk memindahkan data dari entity ke UI

--- app/src/main/java/com/example/ucp2/ui/viewmodelmk/DetailMKViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodelmk/DetailMKViewModel.kt
@@ -2,6 +2,18 @@ package com.example.ucp2.ui.viewmodelmk
 
 import com.example.ucp2.data.entity.MataKuliah
 
+
+// Memindahkan data dari entity ke UI
+fun MataKuliah.toDetailUiEvent(): MataKuliahEvent {
+    return MataKuliahEvent(
+        kode = kode,
+        nama = nama,
+        sks = sks,
+        semester = semester,
+        dosenPengampu = dosenPengampu
+    )
+}
+
 fun MataKuliahEvent.toMataKuliahEntity(): MataKuliah = MataKuliah(
     kode = kode,
     nama = nama,

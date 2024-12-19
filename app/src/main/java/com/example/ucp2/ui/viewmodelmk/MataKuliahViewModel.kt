759fdeb74d76f2ec5fd33ccf372b2e09fe5aa0d3
hilal555 <muhamadhilal04@gmail.com>
2024-12-19 14:20:34 +0700

    Membuat file MataKuliahViewModel

---
+++ app/src/main/java/com/example/ucp2/ui/viewmodelmk/MataKuliahViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.ucp2.ui.viewmodelmk
+
+import androidx.lifecycle.ViewModel
+import com.example.ucp2.repository.RepositoryMK
+
+class MataKuliahViewModel(private val repositoryMK: RepositoryMK) : ViewModel()
+{
+
+
+}

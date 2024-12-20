5650164bcca2e59a45e0be700a1f1c9f2d1b6c7b
hilal555 <muhamadhilal04@gmail.com>
2024-12-20 21:57:17 +0700

    integrasi navigasi untuk mengelola halaman utama,dosen dan mk

--- app/src/main/java/com/example/ucp2/MainActivity.kt
+++ app/src/main/java/com/example/ucp2/MainActivity.kt
@@ -11,8 +11,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.ucp2.ui.navigation.PengelolaHalamanDosen
+import com.example.ucp2.ui.navigation.PengelolaHalamanMK
 import com.example.ucp2.ui.theme.UCP2Theme
+import com.example.ucp2.ui.view.HalamanUtama
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,14 +26,28 @@ class MainActivity : ComponentActivity() {
         setContent {
             UCP2Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    PengelolaHalamanDosen(
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    val navController = rememberNavController()
+                    NavHost(
+                        navController = navController,
+                        startDestination = "Utama",
+                        modifier = Modifier.padding(innerPadding) //
+                    ) {
+                        composable("Utama") {
+                            HalamanUtama(navController)
+                        }
+                        composable("Dosen") {
+                            PengelolaHalamanDosen()
+                        }
+                        composable("MK") {
+                            PengelolaHalamanMK()
+                        }
+                    }
                 }
             }
         }
     }
 }
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {

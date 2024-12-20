e69195b095cf152b94abb8827041364adf3ea2ed
hilal555 <muhamadhilal04@gmail.com>
2024-12-20 22:56:05 +0700

    Memodifikasi tampilan halaman utama

--- app/src/main/java/com/example/ucp2/ui/view/HalamanUtama.kt
+++ app/src/main/java/com/example/ucp2/ui/view/HalamanUtama.kt
@@ -1,32 +1,169 @@
 package com.example.ucp2.ui.view
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.ucp2.R
 
 @Composable
 fun HalamanUtama(navController: NavController,
-                 modifier: Modifier = Modifier){
-    Column (
-        Modifier.fillMaxSize(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
-    ){
-        Button(onClick = {
-            navController.navigate("Dosen")
-        }) {
-            Text(text = "Dosen")
+                 modifier: Modifier = Modifier) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                color = colorResource(
+                    id = R.color.primary
+                )
+            )
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(30.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                modifier = Modifier.size(110.dp),
+                painter = painterResource(
+                    id = R.drawable.umy
+                ),
+                contentDescription = ""
+            )
+
+            Spacer(modifier = Modifier.padding(start = 20.dp))
+
+            Column {
+                Text(
+                    text = "Universitas Muhammadiyah Yogyakarta",
+                    color = Color.White,
+                    fontSize = 15.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    text = "Teknologi Informasi",
+                    color = Color.White,
+                    fontWeight = FontWeight.Light
+                )
+            }
         }
-        Button(onClick = {
-            navController.navigate("MK")
-        }) {
-            Text(text = "Matakuliah")
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(
+                    color = Color.White,
+                    shape = RoundedCornerShape(
+                        topEnd = 15.dp,
+                        topStart = 15.dp
+                    )
+                )
+        ){
+
+            Column (
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ){
+                Text(
+                    text = "Pengelolaan Data Dosen dan Matakuliah",
+                    color = Color.Black,
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                Button(
+                    onClick = {
+                        navController.navigate("Dosen")
+                    },
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color(0xFF194A7A),
+                        contentColor = Color.White
+                    ),
+                    modifier = Modifier.padding(8.dp)
+                        .height(50.dp)
+                        .fillMaxWidth(),
+                    shape = RoundedCornerShape(12.dp)
+                ) {
+                    Row(
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+
+                        Text(
+                            text = "Dosen",
+                            fontSize = 18.sp,
+                            modifier = Modifier.weight(1f),
+                            textAlign = TextAlign.Start
+                        )
+                        Icon(
+                            imageVector = Icons.Default.KeyboardArrowRight,
+                            contentDescription = "Arrow End",
+                            tint = Color.White
+                        )
+                    }
+                }
+
+                Button(
+                    onClick = {
+                        navController.navigate("MK")
+                    },
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color(0xFF476F95),
+                        contentColor = Color.White
+                    ),
+                    modifier = Modifier.padding(8.dp)
+                        .height(50.dp)
+                        .fillMaxWidth(),
+                    shape = RoundedCornerShape(12.dp)
+                ) {
+                    Row(
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Text(
+                            text = "Matakuliah",
+                            fontSize = 18.sp,
+                            modifier = Modifier.weight(1f),
+                            textAlign = TextAlign.Start
+                        )
+                        Icon(
+                            imageVector = Icons.Default.KeyboardArrowRight,
+                            contentDescription = "Arrow End",
+                            tint = Color.White
+                        )
+                    }
+                }
+
+            }
         }
     }
 }

e0e5f5d5d007fd235838a2d96f95cf9c9a057d01
hilal555 <muhamadhilal04@gmail.com>
2024-12-22 01:59:59 +0700

    mengubah warna button simpan

--- app/src/main/java/com/example/ucp2/ui/view/mk/InsertMKView.kt
+++ app/src/main/java/com/example/ucp2/ui/view/mk/InsertMKView.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.OutlinedTextField
@@ -24,9 +25,11 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.ucp2.R
 import com.example.ucp2.ui.customwidget.CustomTopAppBar
 import com.example.ucp2.ui.navigation.AlamatNavigasi
 import com.example.ucp2.ui.viewmodeldosen.PenyediaViewModel
@@ -127,8 +130,13 @@ fun InsertBodyMatkul(
         )
         Button(
             onClick = onClick,
-            modifier = Modifier.fillMaxWidth()
-
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = colorResource(id = R.color.primary),
+                contentColor = Color.White
+            )
         ) {
             Text("Simpan")
         }

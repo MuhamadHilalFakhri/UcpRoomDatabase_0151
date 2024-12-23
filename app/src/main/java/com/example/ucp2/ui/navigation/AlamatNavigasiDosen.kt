48b73203b12a214baffc4eed8e7fbad30e1044ca
hilal555 <muhamadhilal04@gmail.com>
2024-12-23 13:05:37 +0700

    Menghapus object DestinasiDetailDosen

--- app/src/main/java/com/example/ucp2/ui/navigation/AlamatNavigasiDosen.kt
+++ app/src/main/java/com/example/ucp2/ui/navigation/AlamatNavigasiDosen.kt
@@ -8,8 +8,3 @@ object DestinasiHomeDosen : AlamatNavigasi {
     override val route = "home_dosen"
 }
 
-object DestinasiDetailDosen : AlamatNavigasi {
-    override val route = "detail_dosen"
-    const val NIDN = "nidn"
-    val routesWithArg = "$route/{$NIDN}"
-}
